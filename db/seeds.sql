e314ca197e1ad309bb09b8c5a7963a62ce769fb4
Landon Evans <evans.landon@gmail.com>
2019-09-08 16:15:25 -0500

    initial upload

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES("Bacon Double Cheeseburger")
+,
+INSERT INTO burgers
+    (burger_name)
+VALUES("The MacDaddy")
+,
+INSERT INTO burgers
+    (burger_name)
+VALUES("Guacamole Bacon Burger")
